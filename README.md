a39b932ab28028651ddf63a728620cbbf0d4b85a
Miroslav Bendík <miroslav.bendik@gmail.com>
2012-07-10 21:38:53 +0200

    Added configurable colors

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ location: `~/.config/LinuxOS.sk/gitwatcher.cfg`
 
     [DEFAULT]
     icon=/usr/share/icons/oxygen/64x64/actions/svn-commit.png
+    color_add=#00ff00
+    color_remove=#ff2000
+    display_time=360000
 
     [test]
     directory=/home/user/gittest/.git
--- gitwatcher.py
+++ gitwatcher.py
@@ -9,19 +9,28 @@ from subprocess import Popen, PIPE
 
 
 class GitWatcher(object):
-	def __init__(self):
-		self.__stat_re = re.compile("([0-9]* )(\+*)(-*)")
+	def __init__(self, options):
+		self.directory = options["directory"]
+		self.icon = options["icon"]
+		self.display_time = int(options.get("display_time", 3600000))
+
+		self.branch_dir = ["refs", "heads"]
+		self.git_command = ["git", "--git-dir", self.directory]
+
+		color_add = options.get("color_add", "#00ff00")
+		color_remove = options.get("color_remove", "#ff0000");
+		self.stat_re = re.compile("([0-9]* )(\+*)(-*)")
+		self.stat_sub = "\\1"+self.wrap_span("\\2", {"color": color_add})+self.wrap_span("\\3", {"color": color_remove})
 
 	def watch(self):
-		self.__git_command = ["git", "--git-dir", self.directory]
-
 		mask = pyinotify.IN_MODIFY | pyinotify.IN_CREATE | pyinotify.IN_MOVED_TO
 
 		self.__wm = WatchManager()
-		self.__wdd = self.__wm.add_watch(os.path.join(self.directory, 'refs', 'heads'), mask, rec = True)
+		self.__wm.add_watch(os.path.join(self.directory, *self.branch_dir), mask, rec = True)
 
 		notifier = ThreadedNotifier(self.__wm, self.process_event)
 		notifier.start()
+		self.display_notify(self.get_commit_title(), self.get_commit_text())
 
 	def process_event(self, event):
 		(name, ext) = os.path.splitext(event.pathname)
@@ -33,7 +42,7 @@ class GitWatcher(object):
 		Popen(["notify-send", "-t", "3600000", "-a", "git", "-i", self.icon, "-u", "low", title, text])
 
 	def call_git(self, arguments):
-		return str(Popen(self.__git_command + arguments, stdin=PIPE, stdout=PIPE, stderr=PIPE).stdout.readall(), encoding="utf-8")
+		return str(Popen(self.git_command + arguments, stdin=PIPE, stdout=PIPE, stderr=PIPE).stdout.readall(), encoding="utf-8")
 
 	def get_git_author(self):
 		return self.call_git(["log", "-1", "--format='%an'"]).strip("\n")
@@ -42,13 +51,22 @@ class GitWatcher(object):
 		return self.call_git(["log", "-1", "--format=%n<b>%s</b>%n%n%b"])
 
 	def get_git_stat(self):
-		return "<span font=\"monospace\">" + self.__stat_re.sub("\\1<span color=\"#00ff00\">\\2</span><span color=\"#ff0000\">\\3</span>", self.call_git(["diff", "--stat", 'HEAD^', 'HEAD'])) + "</span>"
+		stat_out = self.call_git(["diff", "--stat", 'HEAD^', 'HEAD'])
+		return self.wrap_span(self.stat_re.sub(self.stat_sub, stat_out), {"font": "monospace"})
 
 	def get_commit_title(self):
 		return "Commit from: " + self.get_git_author() + ", project: " + self.project
 
 	def get_commit_text(self):
 		return self.get_git_description() + "\n" + self.get_git_stat()
+
+	def wrap_span(self, text, attributes = {}):
+		return '<span' + self.render_attributes(attributes) + '>' + text + '</span>'
+
+	def render_attributes(self, attributes):
+		if not attributes:
+			return ""
+		return " " + " ".join(map(lambda a: a[0] + '="' + a[1] + '"', attributes.items()))
 
 
 
@@ -59,9 +77,7 @@ def main():
 	watchers = {}
 	for section in config.sections():
 		options = dict(config.items(section))
-		watcher = GitWatcher()
-		watcher.directory = options["directory"]
-		watcher.icon = options["icon"]
+		watcher = GitWatcher(options)
 		watcher.project = section
 		watcher.watch()
 		watchers[section] = watcher
